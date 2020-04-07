d1b3d9a14142076e6e6ed176707bf654885290df
Vignesh Paramasivam <>
2020-04-07 10:14:43 +0530

    Login page - Fixing inconsistent click issue

--- src/test/java/testcore/pages/desktop/DesktopLoginPage.java
+++ src/test/java/testcore/pages/desktop/DesktopLoginPage.java
@@ -38,6 +38,11 @@ public class DesktopLoginPage extends LoginPage {
         getTextboxControl("email").enterText(getTestData().get("Email"));
         getTextboxControl("password").enterText(getTestData().get("Password"));
         getButtonControl("LOGIN").click();
+        assertPageLoad();
+        //Try again if the login click is not performed properly
+        if(driver().findElements(By.xpath("//zui-button[@ng-reflect-label='LOGIN'][@ng-reflect-disabled='false']")).size() > 0) {
+            getButtonControl("LOGIN").click();
+        }
         this.getAgent().getWaiter().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section.navBarContainer")));
         return new HomePage(getConfig(), getAgent(), getTestData()).createInstance();
     }
